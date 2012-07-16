0aa5095fb89a3899817f5f6c47ec35a4fd7ded6d
Andreas Flack <flack@contentcontrol-berlin.de>
2012-07-16 15:37:13 +0200

    Be a bit more careful about paths

--- web/rootfile.php
+++ web/rootfile.php
@@ -1,6 +1,8 @@
 <?php
-define('OPENPSA2_THEME_ROOT', __DIR__ . '/../themes/');
-define('MIDCOM_ROOT', __DIR__ . '/../vendor/openpsa/midcom/lib');
+$basedir = dirname(__DIR__) . '/';
+
+define('OPENPSA2_THEME_ROOT', $basedir . 'themes/');
+define('MIDCOM_ROOT', $basedir . 'vendor/openpsa/midcom/lib');
 define('OPENPSA2_PREFIX', '/');
 
 header('Content-Type: text/html; charset=utf-8');
@@ -9,7 +11,7 @@ $GLOBALS['midcom_config_local'] = array();
 $GLOBALS['midcom_config_local']['person_class'] = 'openpsa_person';
 
 //This file you have to provide
-include '../config.inc.php';
+include $basedir . 'config.inc.php';
 
 // Include the MidCOM environment
 require MIDCOM_ROOT . '/midcom.php';
