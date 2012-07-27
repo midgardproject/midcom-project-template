1f0e9434700f8e5938400fb0e9bac3b5c681ff59
Henri Bergius <henri.bergius@iki.fi>
2012-07-27 16:06:42 +0200

    Set up the Midgard sharedir via env before installing

--- setup/vagrant/manifests/midcom.pp
+++ setup/vagrant/manifests/midcom.pp
@@ -8,6 +8,7 @@ class { 'midcom':
 exec { 'composer_install':
   command => '/usr/local/bin/composer install',
   cwd => '/midcom',
+  environment => 'MIDGARD_ENV_GLOBAL_SHAREDIR=/midcom/config/share',
   require => [
     Exec['download_composer'],
     Package['php5-cli', 'php5-midgard2']
